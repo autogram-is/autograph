62d5e02a82bdeaf41a8abe37c50848ed851482f1
Jeff Eaton <jeff@eaton.fyi>
2022-07-25 09:42:06 -0500

    Better handling of undefined return

--- tests/graph.ts
+++ tests/graph.ts
@@ -17,6 +17,6 @@ test('Nodes and edges persist', () => {
   expect(g.db.prepare('SELECT COUNT(1) FROM node').pluck().get()).toBe(2);
   expect(g.db.prepare('SELECT COUNT(1) FROM edge').pluck().get()).toBe(1);
 
-  const n1Loaded = g.getNode(n1.id) as Node;
+  const n1Loaded = g.getNode(n1.id) ?? {};
   expect(n1).toMatchObject(n1Loaded);
 });
