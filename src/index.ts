38b5da7252412874759327fb38cf4d14edbc19fb
Jeff Eaton <jeff@eaton.fyi>
2022-07-25 20:48:12 -0500

    Given the importance, export bsql's types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export {Entity} from './entity';
 export {Node} from './node';
 export {Edge} from './edge';
 export {Graph} from './graph';
+export {Where, WhereBuilder} from './sql';
+export {Database, Statement} from 'better-sqlite3';
