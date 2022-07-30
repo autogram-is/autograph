cb99ba3eb261aa6c22c35bbe22b752c725edd42f
Jeff Eaton <jeff@eaton.fyi>
2022-07-30 16:45:19 -0500

    Need to actually export it, champ

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export {Entity} from './entity';
 export {Node} from './node';
 export {Edge} from './edge';
 export {Graph} from './graph';
+export {View} from './view';
 export {Where, WhereBuilder} from './sql';
 export {Database, Statement} from 'better-sqlite3';
