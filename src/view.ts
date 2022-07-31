ed570102c766e0cfd9cc2f4e1effb79081dc32a8
Jeff Eaton <jeff@eaton.fyi>
2022-07-30 23:24:39 -0500

    Removing the fallbact 'get'

--- src/view.ts
+++ src/view.ts
@@ -10,22 +10,6 @@ export abstract class View {
   abstract readonly viewName: string;
   abstract ensureView(): boolean;
 
-  get<T extends Entity | Record<string, unknown> = Entity>(
-    clauses?: WhereBuilder,
-    limit = 0
-  ): T[] {
-    const sql = `
-      SELECT data FROM ${this.viewName}
-      ${clauses?.sql}
-      ${limit > 0 ? 'LIMIT ' + limit : ''};
-    `;
-
-    return this.graph.db
-      .prepare(sql)
-      .all(clauses?.parameters)
-      .map(r => JSON.parse(r) as T);
-  }
-
   count(): number {
     const stmt = this.graph.db.prepare(
       `SELECT COUNT(1) FROM ${this.viewName};`
