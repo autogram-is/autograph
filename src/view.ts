1456cba6faa9570a63f2c994987f89145e6a270c
Jeff Eaton <jeff@eaton.fyi>
2022-08-02 00:31:12 -0500

    Making Views nicely filterable

--- src/view.ts
+++ src/view.ts
@@ -1,4 +1,5 @@
 import {Graph} from './graph';
+import {Where, WhereBuilder} from './sql';
 
 export abstract class View {
   constructor(protected graph: Graph) {
@@ -6,11 +7,12 @@ export abstract class View {
   }
 
   abstract readonly viewName: string;
-  abstract ensureView(): boolean;
+  abstract ensureView(): void;
 
-  count(): number {
+  count(where: WhereBuilder = Where()): number {
     const stmt = this.graph.db.prepare(
-      `SELECT COUNT(1) FROM ${this.viewName};`
+      `SELECT COUNT(1) FROM ${this.viewName}
+       WHERE ${where.toString()};`
     );
     return stmt.pluck().get() as number;
   }
