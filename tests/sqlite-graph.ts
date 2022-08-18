d94ae211ed95756764d188974b82a597dffcb429
Jeff Eaton <jeff@eaton.fyi>
2022-08-18 17:05:29 -0500

    Population tests for SqlGraph

--- tests/sqlite-graph.ts
+++ tests/sqlite-graph.ts
@@ -1,7 +1,35 @@
 import test from 'ava';
 import { SqliteGraph } from '../source/graph/index.js';
+import { Node, Edge } from '../source/entity/index.js';
 
-test('graph instantiation works', (t) => {
-  const g = new SqliteGraph();
-  t.notThrows(async () => g.load());
+const g = new SqliteGraph();
+
+test('graph population', (t) => {
+  const nodeCount = 100;
+  const edgeCount = 400;
+
+  const nodes: Node[] = [];
+  for (let i = 1; i < nodeCount; i++) {
+    const n = new Node();
+    n.customProperty = Math.random()
+      .toString(36)
+      .replace(/[^a-z]+/g, '')
+      .slice(0, 5);
+    nodes.push(n);
+  }
+
+  const edges: Edge[] = [];
+  for (let i = 1; i < edgeCount; i++) {
+    const edge = new Edge(
+      nodes[Math.floor(Math.random() * nodes.length)],
+      'knows_of',
+      nodes[Math.floor(Math.random() * nodes.length)],
+    );
+    edges.push(edge);
+  }
+
+  g.set(nodes);
+  g.set(edges);
+
+  t.assert((g.getNode(nodes[0].id)!.id = nodes[0].id));
 });
